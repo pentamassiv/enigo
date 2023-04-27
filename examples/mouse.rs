da43db7d8418566113371a60f54ba39f2e929911
pentamassiv <pentamassiv@posteo.de>
2023-04-27 22:32:50 +0200

    Simplify example

--- examples/mouse.rs
+++ examples/mouse.rs
@@ -9,42 +9,5 @@ fn main() {
     // let mut enigo = enigo::XConnection::default();
 
     thread::sleep(Duration::from_secs(4));
-    println!("screen dimensions: {:?}", enigo.main_display_size());
-    println!("mouse location: {:?}", enigo.mouse_location());
-
-    thread::sleep(wait_time);
-
-    enigo.mouse_move_to(500, 200);
-    thread::sleep(wait_time);
-    println!("mouse location: {:?}", enigo.mouse_location());
-
-    enigo.mouse_down(MouseButton::Left);
-    thread::sleep(wait_time);
-
-    enigo.mouse_move_relative(100, 100);
-    thread::sleep(wait_time);
-    println!("mouse location: {:?}", enigo.mouse_location());
-
-    enigo.mouse_up(MouseButton::Left);
-    thread::sleep(wait_time);
-
     enigo.mouse_click(MouseButton::Left);
-    thread::sleep(wait_time);
-
-    enigo.mouse_scroll_x(2);
-    thread::sleep(wait_time);
-    println!("mouse location: {:?}", enigo.mouse_location());
-
-    enigo.mouse_scroll_x(-2);
-    thread::sleep(wait_time);
-    println!("mouse location: {:?}", enigo.mouse_location());
-
-    enigo.mouse_scroll_y(2);
-    thread::sleep(wait_time);
-    println!("mouse location: {:?}", enigo.mouse_location());
-
-    enigo.mouse_scroll_y(-2);
-    thread::sleep(wait_time);
-
-    println!("mouse location: {:?}", enigo.mouse_location());
 }

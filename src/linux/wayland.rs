5a7efc5ea6a325a8448a21a8a28202e78da4d56a
pentamassiv <pentamassiv@posteo.de>
2023-05-01 21:22:35 +0200

    Added wayland-protocols-plasma

--- src/linux/wayland.rs
+++ src/linux/wayland.rs
@@ -14,12 +14,12 @@ use wayland_client::{
 use wayland_protocols_misc::zwp_virtual_keyboard_v1::client::{
     zwp_virtual_keyboard_manager_v1, zwp_virtual_keyboard_v1,
 };
+use wayland_protocols_plasma::fake_input::client::org_kde_kwin_fake_input;
 use wayland_protocols_wlr::virtual_pointer::v1::client::{
     zwlr_virtual_pointer_manager_v1, zwlr_virtual_pointer_v1,
 };
 
-use xkbcommon::xkb::keysym_get_name;
-use xkbcommon::xkb::{keysym_from_name, keysyms, KEY_NoSymbol, KEYSYM_NO_FLAGS};
+use xkbcommon::xkb::{keysym_from_name, keysym_get_name, keysyms, KEYSYM_NO_FLAGS};
 
 use super::{ConnectionError, Keysym};
 use crate::{Key, KeyboardControllable, MouseButton, MouseControllable};
@@ -92,6 +92,13 @@ impl Con {
         // Setup virtual pointer
         let Some(vp_mgr) = state.pointer_manager.as_ref()else{return Err(ConnectionError::General("No VPMgr".to_string()))};
         let virtual_pointer = vp_mgr.create_virtual_pointer(state.seat.as_ref(), &qh, ());
+
+        // Try to authenticate for the KDE Fake Input protocol
+        if let Some(kde_input) = &state.kde_input {
+            let application = "enigo".to_string();
+            let reason = "enter keycodes or move the mouse".to_string();
+            kde_input.authenticate(application, reason);
+        }
 
         // Only keycodes from 8 to 255 can be used
         let keymap = HashMap::with_capacity(255 - 7);
@@ -865,6 +872,7 @@ impl MouseControllable for Con {
 struct WaylandState {
     keyboard_manager: Option<zwp_virtual_keyboard_manager_v1::ZwpVirtualKeyboardManagerV1>,
     pointer_manager: Option<zwlr_virtual_pointer_manager_v1::ZwlrVirtualPointerManagerV1>,
+    kde_input: Option<org_kde_kwin_fake_input::OrgKdeKwinFakeInput>,
     seat: Option<wl_seat::WlSeat>,
     /*  output: Option<wl_output::WlOutput>,
     width: i32,
@@ -876,6 +884,7 @@ impl WaylandState {
         Self {
             keyboard_manager: None,
             pointer_manager: None,
+            kde_input: None,
             seat: None,
             /*  output: None,
             width: 0,
@@ -930,6 +939,16 @@ impl Dispatch<wl_registry::WlRegistry, ()> for WaylandState {
                     );
                     state.pointer_manager = Some(manager);
                 }
+                "org_kde_kwin_fake_input" => {
+                    let kde_input = registry
+                        .bind::<org_kde_kwin_fake_input::OrgKdeKwinFakeInput, _, _>(
+                            name,
+                            1,
+                            qh,
+                            (),
+                        );
+                    state.kde_input = Some(kde_input);
+                }
                 _ => {}
             }
         }
@@ -959,6 +978,20 @@ impl Dispatch<zwp_virtual_keyboard_v1::ZwpVirtualKeyboardV1, ()> for WaylandStat
         _qh: &QueueHandle<Self>,
     ) {
         // println!("Got a virtual keyboard event {event:?}");
+    }
+}
+
+impl Dispatch<org_kde_kwin_fake_input::OrgKdeKwinFakeInput, ()> for WaylandState {
+    fn event(
+        _state: &mut Self,
+        _vk: &org_kde_kwin_fake_input::OrgKdeKwinFakeInput,
+        _event: org_kde_kwin_fake_input::Event,
+        _: &(),
+        _: &Connection,
+        _qh: &QueueHandle<Self>,
+    ) { // This should never happen, as there are no events specified for this
+         // in the protocol
+         // println!("Got a plasma fake input event {event:?}");
     }
 }
 

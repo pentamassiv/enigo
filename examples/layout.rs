9eeee9f8b75ef80497d728e2deff9720803f2b98
pentamassiv <pentamassiv@posteo.de>
2023-09-02 02:31:20 +0200

    Change order of pressed keys to make it easier to check if all were entered

--- examples/layout.rs
+++ examples/layout.rs
@@ -6,12 +6,12 @@ fn main() {
     thread::sleep(Duration::from_secs(4));
     let mut enigo = Enigo::new();
 
-    enigo.key_click(Key::PageDown);
+    //enigo.key_click(Key::PageDown);
     enigo.key_click(enigo::Key::UpArrow);
     enigo.key_click(enigo::Key::UpArrow);
+    enigo.key_click(enigo::Key::LeftArrow);
+    enigo.key_click(enigo::Key::LeftArrow);
     enigo.key_click(enigo::Key::DownArrow);
-    enigo.key_click(enigo::Key::LeftArrow);
-    enigo.key_click(enigo::Key::LeftArrow);
     enigo.key_click(enigo::Key::RightArrow);
     enigo.key_sequence("𝕊"); // Special char which needs two u16s to be encoded
 }

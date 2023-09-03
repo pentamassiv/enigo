07c5148f74796fc9b846b6b8867517f1fc5fe1ae
pentamassiv <pentamassiv@posteo.de>
2023-09-04 01:18:10 +0200

    Check if Control just does not work as a first

--- examples/keyboard.rs
+++ examples/keyboard.rs
@@ -8,15 +8,6 @@ fn main() {
     // select all
     thread::sleep(Duration::from_secs(5));
 
-    enigo.key_down(Key::Control);
-    enigo.key_click(Key::Layout('a'));
-    enigo.key_up(Key::Control);
-    thread::sleep(Duration::from_secs(5));
-    // write text
-    enigo.key_sequence("Hello World! here is a lot of text  ❤️");
-    enigo.key_sequence("💣💩🔥");
-
-    thread::sleep(Duration::from_secs(5));
     // select all
     enigo.key_down(Key::Control);
     enigo.key_click(Key::Layout('a'));

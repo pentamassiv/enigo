977ff02a1ef681d0caeb647cc36821720105362f
pentamassiv <pentamassiv@posteo.de>
2023-09-03 22:35:45 +0200

    Change example to better test wayland functionallity

--- examples/keyboard.rs
+++ examples/keyboard.rs
@@ -3,10 +3,11 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::sleep(Duration::from_secs(2));
     let mut enigo = Enigo::new();
 
     // select all
-    enigo.key_click(Key::UpArrow);
-    enigo.key_click(Key::LeftArrow);
+    thread::sleep(Duration::from_secs(10));
+    enigo.key_click(Key::Layout('a'));
+    thread::sleep(Duration::from_secs(10));
+    enigo.key_click(Key::Layout('a'));
 }

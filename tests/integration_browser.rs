97617889df8e112ad011e1ef16d579cae0778593
pentamassiv <91755244+pentamassiv@users.noreply.github.com>
2024-09-09 01:15:23 +0200

    CI: Test the mouse simulation as well

--- tests/integration_browser.rs
+++ tests/integration_browser.rs
@@ -19,20 +19,20 @@ fn integration_browser_events() {
     enigo.key(Key::Backspace, Press).unwrap();
     enigo.key(Key::Backspace, Release).unwrap();
 
-    println!("Test mouse"); /*
-                            enigo.button(Button::Left, Click).unwrap();
-                            enigo.move_mouse(100, 100, Abs).unwrap();
-                            enigo.move_mouse(200, 200, Abs).unwrap();
-                            // let (x, y) = enigo.location().unwrap();
-                            // assert_eq!((200, 200), (x, y));
-                            // Relative moves fail on Windows
-                            // For some reason the values are wrong
-                            // enigo.move_mouse(20, 20, Rel).unwrap();
-                            // enigo.move_mouse(-20, 20, Rel).unwrap();
-                            // enigo.move_mouse(20, -20, Rel).unwrap();
-                            // enigo.move_mouse(-20, -20, Rel).unwrap();
-                            // enigo.scroll(1, Vertical).unwrap();
-                            // enigo.scroll(1, Horizontal).unwrap(); Fails on Windows
-                            enigo.main_display().unwrap();
-                            enigo.location().unwrap(); */
+    println!("Test mouse");
+    enigo.button(Button::Left, Click).unwrap();
+    enigo.move_mouse(100, 100, Abs).unwrap();
+    enigo.move_mouse(200, 200, Abs).unwrap();
+    let (x, y) = enigo.location().unwrap();
+    assert_eq!((200, 200), (x, y));
+    // Relative moves fail on Windows
+    // For some reason the values are wrong
+    enigo.move_mouse(20, 20, Rel).unwrap();
+    enigo.move_mouse(-20, 20, Rel).unwrap();
+    enigo.move_mouse(20, -20, Rel).unwrap();
+    enigo.move_mouse(-20, -20, Rel).unwrap();
+    enigo.scroll(1, Vertical).unwrap();
+    enigo.scroll(1, Horizontal).unwrap(); // Fails on Windows
+    enigo.main_display().unwrap();
+    enigo.location().unwrap();
 }

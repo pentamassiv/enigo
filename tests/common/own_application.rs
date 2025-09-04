e869cc4c3e14d06facb10a6e8e4de73b71cca250
pentamassiv <pentamassiv@posteo.de>
2025-09-05 00:49:40 +0200

    wip

--- tests/common/own_application.rs
+++ tests/common/own_application.rs
@@ -1,11 +1,21 @@
 use std::io::{Seek as _, SeekFrom, Write as _};
 
-use enigo::{Axis, Coordinate, Direction, Key};
+use enigo::{Axis, Button, Coordinate, Direction, Enigo, Key, agent::Token};
 use tao::{
-    event::{DeviceEvent, Event, WindowEvent},
+    dpi::PhysicalPosition,
+    event::{DeviceEvent, ElementState, Event, MouseButton, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
+    keyboard::ModifiersState,
+    platform::run_return::EventLoopExtRunReturn as _,
     window::WindowBuilder,
 };
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum Todo {
+    Quit,
+    Noop,
+    Success,
+}
 
 /// This application only displays a white window that is maximized and has no
 /// header bar. It's only purpose is to register input and write it to the file
@@ -18,7 +28,11 @@ use tao::{
 /// very first line with the dimensions of the screen. Afterwards it simulates
 /// input and then deserializes the events from the file to compare them with
 /// the list of events it expected.
-pub struct EnigoApp {}
+pub struct EnigoApp {
+    event_loop: EventLoop<()>,
+    enigo: Enigo,
+    modifier_state: ModifiersState,
+}
 
 impl EnigoApp {
     pub fn new(settings: &enigo::Settings) -> Self {
@@ -30,83 +44,50 @@ impl EnigoApp {
             .expect("unable to create the window");
         window.set_maximized(true);
 
-        let log_file_path = "event_log.txt";
-        let mut log_file = std::fs::OpenOptions::new()
-            .create(true)
-            .write(true)
-            .truncate(true) // Clear the file content
-            .open(log_file_path)
-            .expect("unable to create the log file");
-
         // Get the window size (logical size)
         if let Some(main_display) = window.primary_monitor() {
             let tao::dpi::PhysicalSize { width, height } = main_display.size();
             println!("MainDisplay({width}, {height})");
-            // writeln!(log_file, "MainDisplay({width}, {height})").unwrap();
         }
 
-        event_loop.run(move |event, _, control_flow| match event {
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                /*
-                    WindowEvent::KeyboardInput { event, .. } => {
-                    log_file.seek(SeekFrom::End(0)).unwrap();
-                    writeln!(log_file, "KeyboardInput: {event:?}").unwrap();
-                }
-                */
-                WindowEvent::CursorMoved { position, .. } => {
-                    log_file.seek(SeekFrom::End(0)).unwrap();
-                    writeln!(
-                        log_file,
-                        "MoveMouse({:?}, {:?}, Abs)",
-                        position.x as i32, position.y as i32
-                    )
-                    .unwrap();
-                }
-                WindowEvent::MouseInput { state, button, .. } => {
-                    log_file.seek(SeekFrom::End(0)).unwrap();
-                    writeln!(log_file, "Button({button:?}, {state:?})").unwrap();
-                }
-                WindowEvent::MouseWheel { delta, .. } => {
-                    match delta {
-                        tao::event::MouseScrollDelta::LineDelta(x, y) => {
-                            if x.abs() <= 0.1 && y.abs() <= 0.1 {
-                                // There was no scroll, so do nothing
-                            } else if x.abs() <= 0.1 && y.abs() > 0.1 {
-                                // Vertical scroll
-                                log_file.seek(SeekFrom::End(0)).unwrap();
-                                writeln!(log_file, "Scroll({:?}, Vertical)", -y).unwrap();
-                            } else if x.abs() > 0.1 && y.abs() <= 0.1 {
-                                // Horizontal scroll
-                                log_file.seek(SeekFrom::End(0)).unwrap();
-                                writeln!(log_file, "Scroll({:?}, Horizontal)", -x).unwrap();
-                            } else {
-                                // Scroll on both axis
-                                panic!("scrolling on both axis is not yet supported")
-                            }
-                        }
-                        tao::event::MouseScrollDelta::PixelDelta(_) => {
-                            todo!("Enigo is currently unable to scroll by pixels")
-                        }
-                        _ => panic!("tao added a new variant"),
-                    };
-                }
-                WindowEvent::ModifiersChanged(state) => {
-                    log_file.seek(SeekFrom::End(0)).unwrap();
-                    writeln!(log_file, "ModifierState({state:?})").unwrap()
-                }
-                WindowEvent::ReceivedImeText(string) => {
-                    log_file.seek(SeekFrom::End(0)).unwrap();
-                    writeln!(log_file, "ReceivedImeText({string:?})").unwrap()
-                }
-                _ => (),
-            },
-            Event::DeviceEvent { event, .. } => {
-                log_file.seek(SeekFrom::End(0)).unwrap();
-                writeln!(log_file, "DeviceEvent::{event:?}").unwrap()
-            }
-            _ => (),
-        });
+        let enigo = Enigo::new(settings).expect("Failed to establish enigo connection");
+
+        Self {
+            event_loop,
+            enigo,
+            modifier_state: ModifiersState::empty(),
+        }
+    }
+
+    fn process(&mut self, token: Token) {
+        let todo = Todo::Noop;
+
+        while todo != Todo::Success {
+            self.event_loop.run_return(|event, _, control_flow| {
+                // *control_flow = ControlFlow::Exit; // ensure we exit after this dispatch
+
+                let todo = match event {
+                    Event::WindowEvent { event, .. } => try_from(event, self.modifier_state),
+                    Event::NewEvents(start_cause) => todo!(),
+                    Event::DeviceEvent {
+                        device_id, event, ..
+                    } => todo!(),
+                    Event::UserEvent(_) => todo!(),
+                    Event::Suspended => todo!(),
+                    Event::Resumed => todo!(),
+                    Event::MainEventsCleared => todo!(),
+                    Event::RedrawRequested(window_id) => todo!(),
+                    Event::RedrawEventsCleared => todo!(),
+                    Event::LoopDestroyed => todo!(),
+                    Event::Opened { urls } => todo!(),
+                    Event::Reopen {
+                        has_visible_windows,
+                        ..
+                    } => todo!(),
+                    _ => todo!(),
+                };
+            });
+        }
     }
 }
 
@@ -114,7 +95,6 @@ impl enigo::Keyboard for EnigoApp {
     // This does not work for all text or the library does not work properly
     fn fast_text(&mut self, text: &str) -> enigo::InputResult<Option<()>> {}
     fn key(&mut self, key: Key, direction: Direction) -> enigo::InputResult<()> {}
-
     fn raw(&mut self, keycode: u16, direction: enigo::Direction) -> enigo::InputResult<()> {}
 }
 
@@ -124,4 +104,147 @@ impl enigo::Mouse for EnigoApp {
     fn scroll(&mut self, length: i32, axis: Axis) -> enigo::InputResult<()> {}
     fn main_display(&self) -> enigo::InputResult<(i32, i32)> {}
     fn location(&self) -> enigo::InputResult<(i32, i32)> {}
+}
+
+fn try_from(event: WindowEvent, before_modifier_state: ModifiersState) -> Todo {
+    match event {
+        WindowEvent::CloseRequested => {
+            println!("close requested");
+            Todo::Quit
+        }
+        WindowEvent::CursorMoved { position, .. } => {
+            println!("MoveMouse({}, {}, Abs)", position.x, position.y);
+            Todo::TestResult(Token::MoveMouse(
+                position.x as i32,
+                position.y as i32,
+                Coordinate::Abs,
+            ))
+        }
+        WindowEvent::MouseInput { state, button, .. } => {
+            let direction = from_state(state);
+            let button = from_mouse_button(button);
+            println!("Button({button:?}, {direction:?})");
+            Todo::TestResult(Token::Button(button, direction))
+        }
+        WindowEvent::MouseWheel { delta, .. } => {
+            let (x, y) = match delta {
+                tao::event::MouseScrollDelta::LineDelta(x, y) => (x as f64, y as f64),
+                tao::event::MouseScrollDelta::PixelDelta(PhysicalPosition { x, y }) => (x, y),
+                _ => unimplemented!("tao added a new variant"),
+            };
+
+            let length;
+            let axis;
+            if x.abs() <= 0.1 && y.abs() <= 0.1 {
+                // There was no scroll, so do nothing
+                return Todo::Noop;
+            } else if x.abs() <= 0.1 && y.abs() > 0.1 {
+                // Vertical scroll
+                length = -y;
+                axis = Axis::Vertical;
+            } else if x.abs() > 0.1 && y.abs() <= 0.1 {
+                // Horizontal scroll
+                length = -x;
+                axis = Axis::Horizontal;
+            } else {
+                // Scroll on both axis
+                panic!("scrolling on both axis is not yet supported")
+            };
+
+            match delta {
+                tao::event::MouseScrollDelta::LineDelta(_, _) => {
+                    println!("Scroll({length}, {axis:?})");
+                    Todo::TestResult(Token::Scroll(length as i32, axis))
+                }
+                tao::event::MouseScrollDelta::PixelDelta(_) => {
+                    #[cfg(all(feature = "platform_specific", target_os = "macos"))]
+                    {
+                        println!("Scroll({length}, {axis:?})");
+                        Todo::TestResult(Token::SmoothScroll(length as i32, axis))
+                    }
+                    #[cfg(not(all(feature = "platform_specific", target_os = "macos")))]
+                    {
+                        panic!("Smooth scrolling is not implemented on this platform")
+                    }
+                }
+                _ => unreachable!("would have paniced in the previous match statement"),
+            }
+        }
+        WindowEvent::ModifiersChanged(after_modifier_state) => {
+            let pressed = after_modifier_state - before_modifier_state;
+            let released = before_modifier_state - after_modifier_state;
+
+            let (key, direction) = match (pressed.is_empty(), released.is_empty()) {
+                (false, true) => (from_modifier_state(pressed), Direction::Press),
+                (true, false) => (from_modifier_state(released), Direction::Release),
+                _ => panic!("expected exactly one modifier to change"),
+            };
+
+            println!("Key({key:?}, {direction:?})");
+            Todo::TestResult(Token::Key(key, direction))
+        }
+        WindowEvent::ReceivedImeText(string) => {
+            println!("Text({string})");
+            Todo::TestResult(Token::Text(string))
+        }
+        WindowEvent::KeyboardInput {
+            device_id: _,
+            event,
+            is_synthetic: _,
+            ..
+        } => todo!(),
+        // Not (yet) relevant events
+        WindowEvent::Touch(_) => Todo::Noop,
+        WindowEvent::AxisMotion { .. } => Todo::Noop,
+        WindowEvent::TouchpadPressure { .. } => Todo::Noop,
+        // Irrelevant events
+        WindowEvent::Resized(_)
+        | WindowEvent::Moved(_)
+        | WindowEvent::Destroyed
+        | WindowEvent::DroppedFile(_)
+        | WindowEvent::HoveredFile(_)
+        | WindowEvent::HoveredFileCancelled
+        | WindowEvent::Focused(_)
+        | WindowEvent::CursorEntered { .. }
+        | WindowEvent::CursorLeft { .. }
+        | WindowEvent::ScaleFactorChanged { .. }
+        | WindowEvent::ThemeChanged(_)
+        | WindowEvent::DecorationsClick => Todo::Noop,
+        _ => panic!("Unknown WindowEvent"),
+    }
+}
+
+fn from_state(state: ElementState) -> Direction {
+    match state {
+        ElementState::Pressed => Direction::Press,
+        ElementState::Released => Direction::Release,
+        _ => unreachable!(), /* ElementState only has two variants but for some reason is marked
+                              * as non-exhaustive */
+    }
+}
+
+fn from_mouse_button(button: MouseButton) -> enigo::Button {
+    match button {
+        MouseButton::Left => Button::Left,
+        MouseButton::Right => Button::Right,
+        MouseButton::Middle => Button::Middle,
+        MouseButton::Other(0) => unimplemented!(), /* TODO: Find out the correct mapping for the */
+        // other values
+        MouseButton::Other(1) => unimplemented!(),
+        MouseButton::Other(2) => unimplemented!(),
+        MouseButton::Other(3) => unimplemented!(),
+        MouseButton::Other(4) => unimplemented!(),
+        _ => unreachable!(), /* MouseButton only has two variants but for some reason is marked
+                              * as non-exhaustive */
+    }
+}
+
+fn from_modifier_state(changed_modifier: ModifiersState) -> Key {
+    match changed_modifier {
+        ModifiersState::SHIFT => Key::Shift,
+        ModifiersState::CONTROL => Key::Control,
+        ModifiersState::ALT => Key::Alt,
+        ModifiersState::SUPER => Key::Meta,
+        other => panic!("unknown modifier {other:?}"),
+    }
 }
